a52506204222bf5835a5a35b9d1154bc3f859646
João Victor da Silva <jv782063@gmail.com>
2025-03-10 09:52:11 -0300

    remove structs file

--- internal/config/parser.go
+++ internal/config/parser.go
@@ -7,6 +7,21 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+type (
+	Config struct {
+		BaseURL  string             `yaml:"baseURL" json:"baseURL"`
+		Requests map[string]Request `yaml:"requests" json:"requests"`
+	}
+
+	Request struct {
+		Url      string            `yaml:"url" json:"url"`
+		Endpoint string            `yaml:"endpoint" json:"endpoint"`
+		Method   string            `yaml:"method" json:"method"`
+		Body     any               `yaml:"body" json:"body"`
+		Headers  map[string]string `yaml:"headers" json:"headers"`
+	}
+)
+
 func LoadConfig(filepath string) (*Config, error) {
 	if filepath == "" {
 		return nil, errors.New("you need the path to the file")
